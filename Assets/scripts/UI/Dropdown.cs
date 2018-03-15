7a57023627d55adc5a60ca16701d2647e8255692
Alexandre "allaze-eroler" Laclare <allaze-eroler@orange.fr>
2018-03-15 18:03:52 +0100

    dressup game project updated
    i added the dropdown menu as will some new features.

---
+++ Assets/scripts/UI/Dropdown.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dropdown : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	public RectTransform container;
+	public bool isOpen;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		container = transform.Find("Container").GetComponent<RectTransform>();
+		isOpen = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		Vector3 scale = container.localScale;
+		scale.y = Mathf.Lerp(scale.y, isOpen ? 1 : 0, Time.deltaTime * 12);
+		container.localScale = scale;
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		isOpen = true;
+	}
+
+		public void OnPointerExit(PointerEventData eventData)
+	{
+		isOpen = false;
+	}
+}
---
+++ Assets/scripts/UI/Dropleft.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dropleft : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	public RectTransform togglelewd;
+	public bool isOpen;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		togglelewd = transform.Find("ToggleLewd").GetComponent<RectTransform>();
+		isOpen = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		Vector3 scale = togglelewd.localScale;
+		scale.x = Mathf.Lerp(scale.x, isOpen ? 1 : 0, Time.deltaTime * 12);
+		togglelewd.localScale = scale;
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		isOpen = true;
+	}
+
+		public void OnPointerExit(PointerEventData eventData)
+	{
+		isOpen = false;
+	}
+}
